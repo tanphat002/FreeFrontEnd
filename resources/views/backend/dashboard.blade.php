deeab2709c619b79d10a16c2cb811e80127ac15a
chickenbadct10 <realmadridfc1995@gmail.com>
2020-03-08 19:14:32 +0700

    Khởi tạo project

---
+++ resources/views/backend/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('backend.layout.master')
+
+@section('title')
+Trang quản trị hệ thống
+@endsection
+@section('content')
+Trang quản trị
+@endsection
+@section('content-sub')
+Quản trị
+@endsection
+@section('title-chucnang')
+Chức năng quản trị hệ thống
+@endsection
+@section('custom-js')
+
+@endsection
